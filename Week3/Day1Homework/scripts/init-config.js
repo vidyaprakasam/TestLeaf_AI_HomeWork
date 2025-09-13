d868995c66a90eb509c24f43a7256bcc57fdf8c8
vidyaprakasam <prakasamvidya@gmail.com>
2025-09-13 17:11:20 +0100

    Delete Week3/Day1Homework/scripts directory

--- Week3/Day1Homework/scripts/init-config.js
+++
@@ -1,8 +0,0 @@
-/**
- * Initialize app configuration
- */
-import { initializeAppConfig } from '../config/configUtils.js';
-
-// Apply config immediately and also on DOM load
-initializeAppConfig();
-console.log('🎨 App config initialized');
